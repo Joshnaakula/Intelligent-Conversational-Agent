8017dd047881538989282453c18da91a7e08efa2
yadamalakavita873 <yadamalakavita873@gmail.com>
2026-01-01 08:13:03 +0530

    Initial commit: SigmaGPT frontend and backend

---
+++ Frontend/src/ChatWindow.jsx
@@ -0,0 +1,97 @@
+import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext,useEffect,useState } from "react";
+import {ScaleLoader} from "react-spinners";
+
+function ChatWindow() {
+    const {prompt,setPrompt,reply,setReply,currThreadId, prevChats, setPrevChats,setNewChat} = useContext(MyContext);
+    const [loading,setLoading]=useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const getReply = async () =>{
+         if(!prompt) return;
+        setLoading(true);
+        setNewChat(false);
+        console.log("message", prompt, "threadId", currThreadId);
+        const options ={
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try{
+           const response= await fetch("http://localhost:8080/api/chat", options);
+           const res= await response.json();
+           console.log(res);
+           setReply(res.reply);
+        }catch(err){
+            console.log(err);
+        }
+        setLoading(false);
+    }
+
+    //Append new Chat to prevchats
+    useEffect(() => {
+        if(prompt && reply){
+            setPrevChats(prevChats => [
+                ...prevChats, {
+                    role: "user",
+                    content: prompt
+                },{
+                    role: "assistant",
+                    content: reply
+                }            
+            ]);
+            setPrompt("");
+        }
+    }, [reply]);
+
+
+    const handleProfileClick = () =>{
+        setIsOpen(!isOpen);
+    }
+    return (
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>SmartGPT <i className="fa-solid fa-chevron-down"></i></span>
+                <div className="userIcon"onClick={handleProfileClick}>
+                    <span className="userIconColour"><i className="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            {
+                isOpen && 
+                <div className="dropDown">
+                    <div className="dropDownItem"><i class="fa-solid fa-cloud-arrow-up"></i> Upgrade Plan</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-gear"></i> Settings</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-arrow-right-from-bracket"></i> Log out</div>
+                </div>
+            }
+            <Chat></Chat>
+            <ScaleLoader color="#fff" loading={loading}></ScaleLoader>
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything"
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                    onKeyDown={(e) => e.key === 'Enter'? getReply(): ''}
+                    >
+                    </input>
+                    <div id="submit" onClick={getReply}>
+                        <i className="fa-solid fa-paper-plane"></i>
+                    </div>
+                </div>
+                    <p className="info">
+                        SmartGPT can make mistakes. Check important info, See cookie preferences.
+                    </p>
+            </div>
+        </div>
+    )
+}
+
+export default ChatWindow;
